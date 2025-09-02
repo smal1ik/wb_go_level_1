9bd3914b076badaf89b82092bb1af4d9e604c162
m.karymov <matveykarymov@gmail.com>
2025-09-02 14:54:35 +0800

    16 task

--- L1_16/main.go
+++ L1_16/main.go
@@ -6,9 +6,9 @@ func quickSort(a []int) []int {
 	if len(a) < 2 {
 		return a
 	}
-	pivot := a[len(a)-1]
+	pivot := a[0]
 	left, right := make([]int, 0), make([]int, 0)
-	for i := 0; i < len(a)-1; i++ {
+	for i := 1; i < len(a); i++ {
 		if a[i] < pivot {
 			left = append(left, a[i])
 		} else {
